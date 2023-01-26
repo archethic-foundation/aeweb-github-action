1279da6d05f8fa559ccb83bea7cdcf7da710e8d9
Samuel Manzanera <samuelmanzanera@protonmail.com>
2023-01-26 11:10:20 +0100

    Adapt with AEWeb lib

--- index.js
+++ index.js
@@ -1,277 +1,160 @@
 import fs from 'fs';
-import archethic from 'archethic';
+import archethic, { Crypto, Utils } from 'archethic';
+import aeweb from 'aeweb';
+
 import path, { resolve } from 'path';
-import zlib from 'zlib'
 import { exit } from 'process';
-import set from 'lodash/set.js'
 
 import core from '@actions/core'
 
-
-const MAX_CONTENT_SIZE = 3_145_728
-const MAX_FILE_SIZE = MAX_CONTENT_SIZE - 45_728
-
+const { deriveAddress } = Crypto
+const { originPrivateKey, fromBigInt, uint8ArrayToHex } = Utils
 
 const handler = async function () {
 
   try {
-    // Derive address and get last transaction index
-    const endpoint = core.getInput('endpoint');
+
     let folderPath = path.normalize(core.getInput('path').endsWith(path.sep) ? core.getInput('path').slice(0, -1) : core.getInput('path'))
 
-    const baseSeed = core.getInput('seed');
-    const baseIndex = await archethic.getTransactionIndex(archethic.deriveAddress(baseSeed, 0), endpoint)
+    // Initialize endpoint connection
+    const endpoint = new URL(core.getInput('endpoint')).origin;
 
-    const refSeed = baseSeed + 'aeweb_ref'
-    const firstRefAddress = archethic.deriveAddress(refSeed, 0)
+    const archethic = new Archethic(endpoint)
+    const aeweb = new AEWeb(archethic)
+
+    // Get seeds
+    const baseSeed = core.getInput('seed');
+    const { refSeed, filesSeed } = getSeeds(seed)
+
+    // Get genesis addresses
+    const baseAddress = deriveAddress(baseSeed, 0)
+    const refAddress = deriveAddress(refSeed, 0)
+    const filesAddress = deriveAddress(filesSeed, 0)
+
     const refIndex = await archethic.getTransactionIndex(firstRefAddress, endpoint)
 
-    const filesSeed = baseSeed + 'aeweb_files'
-    const firstFilesAdress = archethic.deriveAddress(filesSeed, 0)
-    let filesIndex = await archethic.getTransactionIndex(firstFilesAdress, endpoint)
+    let filesIndex = await archethic.getTransactionIndex(firstFilesAddress, endpoint)
 
     // Convert directory structure into array of file content
     console.log('Creating file structure and compress content...')
-
-    let argStats
-    const files = []
-    try {
-      argStats = fs.statSync(folderPath)
-      if (argStats.isDirectory()) {
-        handleDirectory(folderPath, files)
-      } else {
-        handleFile(folderPath, files)
-        folderPath = path.dirname(folderPath)
-      }
-
-      if (files.length === 0) {
-        throw { message: 'Folder ' + folderPath + ' is empty' }
-      }
-    } catch (e) {
-      throw e.message
-    }
-
-    // Control size of json content
-    console.log('Spliting file(s) content in multiple transaction if necessary ...')
-
-    const refContent = {}
-    let transactions = []
-    files.sort((a, b) => b.size - a.size)
-    // Loop until all files are stored inside a transaction content
-    while (files.length > 0) {
-      // Get next tx address
-      let txAddress = archethic.deriveAddress(filesSeed, filesIndex + 1)
-
-      let txContent = {}
-      let txContentSize = 0
-      let file = {}
-      // Loop over files to create a content of Max size
-      // Create a transaction when there is no more file to fill the current tx content
-      while (file) {
-        // Take the first file that can fill the content or the first file if content is empty
-        file = txContentSize == 0 ? (
-          files[0]
-        ) : (
-          files.find(elt => (txContentSize + elt.size) <= MAX_FILE_SIZE)
-        )
-
-        if (file) {
-          // Create json file path
-          const tab_path = file.path.replace(folderPath, '').split(path.sep)
-          if (tab_path[0] === '') { tab_path.shift() }
-          let content = file.content
-          const refFileContent = {
-            encodage: file.encodage,
-            address: []
-          }
-
-          // Handle file over than Max size. The file is splited in multiple transactions,
-          // firsts parts take a full transaction, the last part follow the normal sized file construction
-          while (content.length > MAX_FILE_SIZE) {
-            // Split the file
-            const part = content.slice(0, MAX_FILE_SIZE)
-            content = content.replace(part, '')
-            // Set the value in txContent
-            set(txContent, tab_path, part)
-            // Update refContent to refer value at txAddress
-            refFileContent.address.push(txAddress)
-            set(refContent, tab_path, refFileContent)
-            // Set the new size of the file
-            file.size -= MAX_FILE_SIZE
-            // Insert content for new transaction
-            transactions.push({ filesIndex, txContent })
-            // Increment filesIndex for next transaction
-            filesIndex++
-            txAddress = archethic.deriveAddress(filesSeed, filesIndex + 1)
-            txContent = {}
-          }
-
-          // Set the value in txContent
-          set(txContent, tab_path, content)
-          // Update refContent to refer value at txAddress
-          refFileContent.address.push(txAddress)
-          set(refContent, tab_path, refFileContent)
-          // Remove file from files
-          files.splice(files.indexOf(file), 1)
-          // Set the new size of txContent
-          txContentSize += file.size
-        }
-      }
-
-      // Insert content for new transaction
-      transactions.push({ filesIndex, txContent })
-
-      // Increment filesIndex for next transaction
-      filesIndex++
-    }
-
-    // Create transaction
-    console.log('Creating transactions, it may take a while...')
-
-    const originPrivateKey = archethic.getOriginKey()
-
-    const refTx = archethic.newTransactionBuilder('hosting')
-      .setContent(JSON.stringify(refContent))
-      .build(refSeed, refIndex)
-      .originSign(originPrivateKey)
-
-    transactions = transactions.map((elt) => {
-      return archethic.newTransactionBuilder('hosting')
-        .setContent(JSON.stringify(elt.txContent))
-        .build(filesSeed, elt.filesIndex)
-        .originSign(originPrivateKey)
+    getFiles(folderPath).forEach(({ filePath, data}) => {
+      aeweb.addFile(filePath, data)
     })
 
-    // Get transactions fees and ask user
-    console.log('Estimating fees...')
+    // Sign files transactions
+    const transactions = aeweb.getFilesTransactions()
+    transactions = transactions.map(tx => {
+      const index = filesIndex
+      filesIndex++
+      return tx.build(filesSeed, index).originSign(originPrivateKey)
+    })
 
-    // Estimate refTx fees
-    const slippage = 1.02
+    const refTransaction = await aeweb.getRefTransactions(transactions)
+    // Sign ref transaction
+    refTx.build(refSeed, refIndex).originSign(originPrivateKey)
 
-    let { fee: fee , rates: rates } = await archethic.getTransactionFee(refTx, endpoint)
-    const refTxFees = +(fee / 100_000_000 * slippage).toFixed(8)
-
-    let filesTxFees = 0
-    // Estimate filesTx fees
-    for (const tx of transactions) {
-      ({ fee: fee } = await archethic.getTransactionFee(tx, endpoint))
-      filesTxFees += +(fee / 100_000_000 * slippage).toFixed(8)
-    }
-
-
-    // Create transfer transactions
-    const transferTx = archethic.newTransactionBuilder('transfer')
-      .addUCOTransfer(firstRefAddress, refTxFees)
-      .addUCOTransfer(firstFilesAdress, filesTxFees)
-      .build(baseSeed, baseIndex)
-      .originSign(originPrivateKey)
-
-    let fees = refTxFees + filesTxFees
-    fees += (await archethic.getTransactionFee(transferTx, endpoint)).fee
-    console.log('Estimated fees: ' + fees + ' UCO')
-    console.log('Estimated fees: ' + fees / 100_000_000 + ' Uco')
-    fees = fees / 100_000_000
-    transactions.unshift(transferTx)
     transactions.push(refTx)
 
-    console.log(
-      'Total Fee Requirement would be : ' +
-      (fees.toFixed(2)  ) +
-      ' UCO ( $ ' +
-      (rates.usd * fees ).toFixed(2) +
-      ' | € ' +
-      (rates.eur * fees  ).toFixed(2) +
-      '), for ' + transactions.length + ' transactions.'
-    )
+    // Estimation of fees
+    const { refTxFees, filesTxFees } = await cli.estimateTxsFees(archethic, transactions)
 
-    //const ok = await yesno({
-      //question: chalk.yellowBright(
-      //  'Do you want to continue. (yes/no)'
-     // ),
-    //});odo
+    // Create transfer transaction to fund the chains
+    const transferTx = archethic.transaction.new()
+         .setType('transfer')
+         .addUCOTransfer(refAddress, refTxFees)
+         .addUCOTransfer(filesAddress, filesTxFees)
 
-    // TODO: implement max fees
-    const ok = true
-    if (ok) {
+    transferTx.build(baseSeed, baseIndex).originSign(originPrivateKey)
+    transactions.unshift(transferTx)
 
-        // Send transactions
+    const { fee, rates } = await archethic.transaction.getTransactionFee(transferTx)
+    const fees = fromBigInt(fee + refTxFees + filesTxFees)
 
-      console.log('Sending ' + transactions.length + ' transactions...')
-      await sendTransaction(transactions, 0, endpoint).then(() => {
-        const url = endpoint + '/api/web_hosting/' + firstRefAddress + '/'
-        core.setOutput("transaction-address", url);
-        console.log('url : ', url);
-      })
-      .catch((e) => {
-        console.log("error :" + e)
-      })
-      
+    console.log(`Total Fee Requirement would be: ${fees} UCO ${(rates.usd * fees).toFixed(2)}, for ${nbTxs} transactions`)
 
-      
+    await sendTransactions(transactions, 0, endpoint).then(() => {
+      console.log(`Website is deployed at: ${endpoint}/api/web_hosting/${uint8ArrayToHex(refAddress)}/`)
       exit(0)
+    })
+    .catch(error => {
+      console.log('Transaction validation error : ' + error)
+      exit(1)
+    })
 
-    } else {
-      throw 'User aborted website deployment.'
-    }
+    exit(0)
   } catch (e) {
     console.log(e)
     exit(1)
   }
 }
 
-async function sendTransaction(transactions, index, endpoint) {
-  return new Promise(async (resolve, reject) => {
-    let sender = archethic.newTransactionSender()
-     sender.on('confirmation', async (nbConf, maxConf) => {
-                console.log('Transaction ' + (index + 1) + ' confirmed once')
-                sender.unsubscribe()
-                if (index  < transactions.length - 1) {
-                  await sendTransaction(transactions, index + 1, endpoint)
-                  resolve();
-                }
-                else {
-                  console.log('All transactions sent')
-                  resolve();
-                }
-
-     })
-
-    sender.on('sent', () => console.log('Transaction ' + (index + 1) + ' sent'))
-    sender.on('error', (context, reason) => {
-      //console.log('Transaction ' + index + ' error : ' + reason + "context : " + context)
-      reject(reason)
-    })
-    sender.send(transactions[index], endpoint)
-  })
-
-  
-  
-}
-
-
-
-
-function handleFile(file, files) {
+function handleFile(aeweb, file, files) {
   const data = fs.readFileSync(file)
-  const content = zlib.gzipSync(data).toString('base64url')
-  files.push({
-    path: file,
-    size: content.length,
-    content,
-    encodage: 'gzip'
-  })
+  aeweb.addFile(file, data) 
 }
 
-function handleDirectory(entry, files) {
+function handleDirectory(aeweb, entry, files) {
   const stats = fs.statSync(entry)
 
   if (stats.isDirectory()) {
     fs.readdirSync(entry).forEach(child => {
-      handleDirectory(entry + path.sep + child, files)
+      handleDirectory(aeweb, entry + path.sep + child, files)
     });
   } else {
-    handleFile(entry, files)
+    handleFile(aeweb, entry, files)
   }
+}
+
+function getSeeds(seed) {
+  return {
+    refSeed: baseSeed + 'aeweb_ref',
+    filesSeed: baseSeed + 'aeweb_files'
+  }
+}
+
+function getFiles(folderPath, includeGitIgnoredFiles = false) {
+  let files = []
+  const filters = []
+  if (fs.statSync(folderPath).isDirectory()) {
+    handleDirectory(folderPath, files, includeGitIgnoredFiles, filters)
+
+    files = files.map((file) => {
+      file.filePath = file.filePath.replace(folderPath, '')
+      return file
+    })
+  } else {
+   const data = fs.readFileSync(folderPath)
+   const file_path = PathLib.basename(folderPath)
+   files.push({ "filePath": file_path, data })
+  }
+
+  return files
+}
+
+async function sendTransactions(transactions, index, endpoint) {
+  return new Promise(async (resolve, reject) => {
+    console.log(`Transaction ${index + 1} ...`)
+    const tx = transactions[index]
+
+    tx
+      .on('requiredConfirmation', async (nbConf) => {
+        console.log('Transaction confirmed !')
+        console.log(`See transaction in explorer: ${endpoint}/explorer/transaction/${uint8ArrayToHex(tx.address)}`)
+        console.log('-----------')
+
+        if (index + 1 == transactions.length) {
+          resolve()
+        } else {
+          sendTransactions(transactions, index + 1, endpoint)
+            .then(() => resolve())
+            .catch(error => reject(error))
+        }
+      })
+      .on('error', (context, reason) => reject(reason))
+      .on('timeout', (nbConf) => reject('Transaction fell in timeout'))
+      .on('sent', () => console.log('Waiting transaction validation...'))
+      .send(75)
+  })
 }
 
 await handler()
