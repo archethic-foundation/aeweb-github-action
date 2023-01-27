b16540559a2ecfd70ba698578d5964c050974821
Samuel Manzanera <samuelmanzanera@protonmail.com>
2023-01-27 16:20:18 +0100

    Ease the testing

---
+++ action.js
@@ -0,0 +1,72 @@
+import Archethic, { Crypto, Utils } from 'archethic';
+import AEWeb from 'aeweb';
+
+import { normalizeFolderPath, getFolderFiles } from './file.js'
+import { estimateTxsFees, getSeeds, sendTransactions } from './utils.js'
+
+const { deriveAddress } = Crypto
+const { originPrivateKey, fromBigInt, uint8ArrayToHex } = Utils
+
+export async function handler(baseSeed, folderPath, endpoint) {
+  const normalizedFolderPath = normalizeFolderPath(folderPath)
+
+  // Initialize endpoint connection
+  const normalizedEndpoint = new URL(endpoint).origin;
+  const archethic = new Archethic(normalizedEndpoint)
+  await archethic.connect()
+
+  const aeweb = new AEWeb(archethic)
+
+  // Get seeds
+  const { refSeed, filesSeed } = getSeeds(baseSeed)
+
+  // Get genesis addresses
+  const baseAddress = deriveAddress(baseSeed, 0)
+  const refAddress = deriveAddress(refSeed, 0)
+  const filesAddress = deriveAddress(filesSeed, 0)
+
+  // Get the chains size
+  const baseIndex = await archethic.transaction.getTransactionIndex(baseAddress)
+  const refIndex = await archethic.transaction.getTransactionIndex(refAddress)
+  let filesIndex = await archethic.transaction.getTransactionIndex(filesAddress)
+
+  // Convert directory structure into array of file content
+  console.log('Creating file structure and compress content...')
+
+  getFolderFiles(normalizedFolderPath).forEach(({ filePath, data}) => {
+    aeweb.addFile(filePath, data)
+  })
+
+  // Sign files transactions
+  const transactions = aeweb.getFilesTransactions().map(tx => {
+    const index = filesIndex
+    filesIndex++
+    return tx.build(filesSeed, index).originSign(originPrivateKey)
+  })
+
+  const refTx = await aeweb.getRefTransaction(transactions)
+  // Sign ref transaction
+  refTx.build(refSeed, refIndex).originSign(originPrivateKey)
+
+  transactions.push(refTx)
+
+  // Estimation of fees
+  const { refTxFees, filesTxFees } = await estimateTxsFees(archethic, transactions)
+
+  // Create transfer transaction to fund the chains
+  const transferTx = archethic.transaction.new()
+    .setType('transfer')
+    .addUCOTransfer(refAddress, refTxFees)
+    .addUCOTransfer(filesAddress, filesTxFees)
+
+  transferTx.build(baseSeed, baseIndex).originSign(originPrivateKey)
+  transactions.unshift(transferTx)
+
+  const { fee, rates } = await archethic.transaction.getTransactionFee(transferTx)
+  const fees = fromBigInt(fee + refTxFees + filesTxFees)
+
+  console.log(`Total Fee Requirement would be: ${fees} UCO ($${(rates.usd * fees).toFixed(2)}) for ${transactions.length} transactions`)
+
+  await sendTransactions(transactions, 0, normalizedEndpoint)
+  console.log(`Website is deployed at: ${normalizedEndpoint}/api/web_hosting/${uint8ArrayToHex(refAddress)}/`)
+}
---
+++ file.js
@@ -0,0 +1,67 @@
+import fs from "fs"
+import path from "path"
+import glob from "glob"
+import parse from "parse-gitignore"
+
+export function normalizeFolderPath(folderPath) {
+  return path.normalize(folderPath.endsWith(path.sep) ? folderPath.slice(0, -1) : folderPath)
+}
+
+export function getFolderFiles(folderPath, includeGitIgnoredFiles = false) {
+  let files = []
+  const filters = []
+  if (fs.statSync(folderPath).isDirectory()) {
+    handleDirectory(folderPath, files, includeGitIgnoredFiles, filters)
+
+    files = files.map((file) => {
+      file.filePath = file.filePath.replace(folderPath, '')
+      return file
+    })
+  } else {
+   const data = fs.readFileSync(folderPath)
+   const filePath = path.basename(folderPath)
+   files.push({ filePath, data })
+  }
+
+  return files
+}
+
+function handleDirectory(folderPath, files, includeGitIgnoredFiles, filters) {
+  if (!includeGitIgnoredFiles) {
+    filters = getFilters(folderPath, filters)
+  }
+
+  // Check if files is filtered
+  if (!filters.includes(folderPath)) {
+    // reduce search space by omitting folders at once
+    if (fs.statSync(folderPath).isDirectory()) {
+      fs.readdirSync(folderPath).forEach((child) => {
+        handleDirectory(folderPath + path.sep + child, files, includeGitIgnoredFiles, filters)
+      })
+    } else {
+      handleFile(folderPath, files);
+    }
+  }
+}
+
+function handleFile(filePath, files) {
+    const data = fs.readFileSync(filePath)
+    files.push({ filePath, data })
+}
+
+function getFilters(folderPath, filters) {
+  let newFilters = []
+
+  const gitIgnoreFilePath = path.join(folderPath, '.gitignore')
+  if (fs.existsSync(gitIgnoreFilePath)) {
+    console.log('Ignore files from: ' + gitIgnoreFilePath)
+    newFilters = parse(fs.readFileSync(gitIgnoreFilePath))['patterns']
+    newFilters.unshift('.gitignore')
+    newFilters.unshift('.git')
+  }
+
+  // Add the new filters to the previous filters
+  return newFilters.reduce((acc, path) => {
+    return acc.concat(glob.sync(path.join(folderPath, path)))
+  }, filters)
+}
