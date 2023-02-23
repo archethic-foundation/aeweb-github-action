5a4c26b0c039763b8d4f85421bfac794c17cfe0e
Samuel Manzanera <samuelmanzanera@protonmail.com>
2023-02-23 13:47:31 +0100

    Add node_modules to .gitignore

--- node_modules/@absinthe/socket/src/notifier/reactivate.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import type {Notifier} from "./types";
-
-const reactivate = <Result, Variables: void | Object>(
-  notifier: Notifier<Result, Variables>
-) => (notifier.isActive ? notifier : {...notifier, isActive: true});
-
-export default reactivate;
